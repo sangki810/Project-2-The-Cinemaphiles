5e06f544d55fa05816dd5e852885d99b1207799a
nikkikhou <nikkikhou@gmail.com>
2022-11-15 14:15:08 -0500

    got axios working

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,25 @@
+const axios =require("axios")
 
+console.log("hello")
 
-window.axios
+axios
   .get("https://api.themoviedb.org/3/movie/popular", {
     params: { api_key: "b8ee7a106909a7a297e5b0ca3b261018", language: "en-US" },
   })
   .then((result) => {
-    console.log(result);
+    console.log(result.data.results);
   });
+
+
+//   const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '67c02957c5msh2e9603507bf3477p1d0597jsn9a3e00581dcf',
+// 		'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://advanced-movie-search.p.rapidapi.com/search/movie?query=kong&page=1', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
