358d1474f5c0bb7ddd0a95e225ac4e4d0629500c
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-15 10:47:45 -0500

    inital homepage routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,32 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Movie } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     res.send("<h1>Cinemafiles Route!</h1>");
 });
+
+router.get('/', async (req, res) => {
+  try {
+    const movieData = await Movie.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name']
+        },
+      ]
+    });
+
+    const movies = movieData.map((post) => post.get({ plain: true }));
+    
+    res.render('homepage', {
+      movies,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
