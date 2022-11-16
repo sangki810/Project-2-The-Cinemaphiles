392c48daa1fcf315cd13040c312e7c01e36af686
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-16 11:22:18 -0500

    added review routes, initial reviews.handlebars content

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -24,8 +24,8 @@ router.get('/', async (req, res) => {
     });
 
     const reviews = reviewData.map((review) => review.get({ plain: true }));
-
-    res.render('reivews', {
+   
+    res.render('reviews', {
       reviews,
       logged_in: req.session.logged_in
     });
@@ -34,10 +34,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-// render a single reviews
+// render a single review
 router.get('/:id', async (req, res) => {
   try{
-    const reviewData = await Review.findByPk({
+    const reviewData = await Review.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -52,6 +52,24 @@ router.get('/:id', async (req, res) => {
       ...review,
       logged_in: req.session.logged_in
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try{
+    const reviewData = await Review.update(
+      req.body,
+      { where: { id: req.params.id } }
+    );
+
+    if (!reviewData) {
+      res.status(404).json({ message: 'No review found with this id!' });
+      return;
+    }
+
+    res.status(200).json(`Review ${req.params.id} has been updated`);
   } catch (err) {
     res.status(500).json(err);
   }
