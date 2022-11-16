d8738ee8ea831c07e3402e03d78028afaf51b53c
nikkikhou <nikkikhou@gmail.com>
2022-11-15 21:34:56 -0500

    added click handler to search bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,11 @@
 // event listener for search bar
-var searchButton = document.querySelector(".search-bar");
-var searchInput = document.querySelector(".input");
+var searchButton = document.querySelector("#searchButton");
+var searchInput = document.querySelector("#searchInput");
 
 var submitMovieSearch = function () {
+  var movieSearch = searchInput.value.trim();
+  console.log(movieSearch);
+  // axios get pass in params
 
-    var movieSearch = searchInput.value.trim();
-}
+};
+searchButton.addEventListener("click", submitMovieSearch);
