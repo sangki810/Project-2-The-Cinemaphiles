11f62146bb7cceb46fbbb656c9abd71bb216ade6
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-11-16 13:42:11 -0500

    added a query selector and event listener for review movies page.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 // event listener for search bar
 var searchButton = document.querySelector("#searchButton");
 var searchInput = document.querySelector("#searchInput");
+var movieReview = document.querySelector("#movieInput");
+
 
 var submitMovieSearch = function () {
   var movieSearch = searchInput.value.trim();
@@ -10,3 +12,9 @@ var submitMovieSearch = function () {
   // window.location.reload(true)
 };
 searchButton.addEventListener("click", submitMovieSearch);
+
+var completeMovieReview = function () {
+  window.location.href = '/reviews';
+    // window.location.reload(true)
+};
+movieReview.addEventListener("click", completeMovieReview);
