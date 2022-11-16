27d84c26a4c7cbe4285a5695ea12674d2512c4c5
nikkikhou <nikkikhou@gmail.com>
2022-11-15 19:33:08 -0500

    created search bar

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// event listener for search bar
+var searchButton = document.querySelector(".search-bar");
+var searchInput = document.querySelector(".input");
+
+var submitMovieSearch = function () {
+
+    var movieSearch = searchInput.value.trim();
+}
